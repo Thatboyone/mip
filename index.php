d46cff86e96a2606c6ccb575636330c97b249a65
Thatboyone <1750216665@qq.com>
2018-08-25 17:02:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+if (!version_compare(PHP_VERSION,'5.4.0','ge')) {
+    header("Content-type: text/html; charset=utf-8");
+    echo '您当前使用的PHP版本为：' . PHP_VERSION . '系统最低要求PHP5.4 建议使用PHP7.0版本！';
+    exit;
+}
+define('MIP_HOST',true);
+define('BAIDU',false);
+defined('MIP_ROOT') or define('MIP_ROOT', __DIR__ . '/');
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/app/');
+define('PUBLIC_PATH', __DIR__ . '/public/');
+define('VENDOR_PATH',__DIR__ . '/system/vendor/');
+// 加载框架引导文件
+require __DIR__ . '/system/thinkphp/start.php';
